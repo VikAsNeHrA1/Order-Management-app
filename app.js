d9d2026e1696a747caa9eadf714c6923a9fad9f7
VikAsNeHrA1 <nehravikas9@gmail.com>
2023-11-04 10:23:01 -0500

    Added OAuth

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 const port = process.env.PORT || 5500;
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URI;
@@ -10,12 +12,19 @@ const uri = process.env.MONGO_URI;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Initialize session management middleware first
+app.use(session({
+  secret: 'f3eGj9k5sD8j1M3cQ0zN6jV4bT7yL2xZ',
+  resave: false,
+  saveUninitialized: false
+}));
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Connect to MongoDB once at the start
 client.connect().then(() => {
   console.log("Connected to MongoDB");
 }).catch(err => {
@@ -23,12 +32,11 @@ client.connect().then(() => {
   process.exit(1);
 });
 
-
 app.get('/', (req, res) => {
   res.render('login');
 });
 
-app.get('/orders', async (req, res) => {
+app.get('/orders', checkUserType('warehouse'), async (req, res) => {
   try {
     const collection = client.db("warehouse").collection("orders");
     let orders = await collection.find().toArray();
@@ -59,15 +67,23 @@ app.get('/order-success', (req, res) => {
 app.post('/orders/update/:id', async (req, res) => {
   try {
     const collection = client.db("warehouse").collection("orders");
+    const updateObject = { orderList: req.body.orderList };
+
+    // Check if status is provided in the request
+    if (req.body.status) {
+        updateObject.status = req.body.status;
+    }
+
     await collection.findOneAndUpdate(
       { _id: new ObjectId(req.params.id) },
-      { $set: { orderList: req.body.orderList } }
+      { $set: updateObject }
     );
-    res.redirect('/orders');
+    res.json({ success: true });
   } catch (err) {
     res.status(500).send("Error updating order");
   }
 });
+
 
 app.post('/orders/delete/:id', async (req, res) => {
   try {
@@ -79,25 +95,75 @@ app.post('/orders/delete/:id', async (req, res) => {
   }
 });
 
-app.post('/login', (req, res) => {
-  
-  const userType = req.body.userType;
 
-  if (userType === 'store') {
-      
-      res.redirect('/store-owner'); 
-  } else if (userType === 'warehouse') {
-      
-      res.redirect('/orders');  
-  } else {
+// For login
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  const collection = client.db("warehouse").collection("users");
+  const user = await collection.findOne({ username: req.body.username });
+  
+  if (user && await bcrypt.compare(req.body.password, user.password)) {
+    // Setting userType in the session to be used for authorization
+    req.session.userType = user.userType;
+    
+    if (user.userType === 'store') {
+      res.redirect('/store-owner');
+    } else if (user.userType === 'warehouse') {
+      res.redirect('/orders');
+    } else {
       res.status(400).send("Invalid user type");
+    }
+  } else {
+    res.status(401).send('Incorrect username or password');
   }
 });
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+  const { username, password, userType } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const collection = client.db("warehouse").collection("users");
+    await collection.insertOne({ username, password: hashedPassword, userType });
+    req.session.userType = userType;
+    res.redirect('/login');
+  } catch (err) {
+    console.error("Error during registration:", err);
+    res.status(500).send("Error registering user");
+  }
+});
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error during logout:", err);
+      res.status(500).send('Error during logout');
+    }
+    res.redirect('/'); // redirect to login
+  });
+});
+
+function checkUserType(requiredType) {
+  return (req, res, next) => {
+    if (req.session && req.session.userType === requiredType) {
+      next();
+    } else {
+      res.status(403).send('Access denied');
+    }
+  }
+}
+
 
 app.get('/store-owner', (req, res) => {
   
   res.render('store-owner'); 
 });
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
